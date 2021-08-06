d99a0b273b9c724279d808029fe3ef3af317153b
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-08-06 20:22:29 +0530

    Fixed A Bug

--- pages/blog/add.js
+++ pages/blog/add.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import fire from "../../config/firebaseConfig";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Head from "next/head";
 const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -17,32 +18,37 @@ const CreatePost = () => {
     router.push("/");
   };
   return (
-    <div className="container">
-      <h2 className="mt-3">Add Blog</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <p className="form-label">Title :</p>
-          <input
-            type="text"
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-            className="form-control my-2"
-          />
-        </div>
-        <div>
-          <p className="form-label">Content :</p>
-          <textarea
-            value={content}
-            onChange={({ target }) => setContent(target.value)}
-            className="form-control"
-          />
-        </div>
-        <button type="submit" className="btn btn-outline-dark my-3">
-          Save
-          <i className="far fa-save ms-2"></i>
-        </button>
-      </form>
-    </div>
+    <>
+      <Head>
+        <title>WeBlog | Add Blog</title>
+      </Head>
+      <div className="container">
+        <h2 className="mt-3">Add Blog</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p className="form-label">Title :</p>
+            <input
+              type="text"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+              className="form-control my-2"
+            />
+          </div>
+          <div>
+            <p className="form-label">Content :</p>
+            <textarea
+              value={content}
+              onChange={({ target }) => setContent(target.value)}
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-dark my-3">
+            Save
+            <i className="far fa-save ms-2"></i>
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 export default CreatePost;
